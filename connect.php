46cb7ccf544d53544858929c6d149fce8e519acf
GabrielMMVI <161396206+GabrielMMVI@users.noreply.github.com>
2024-06-14 20:59:28 -0300

    Connect feito

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$dbFile = 'bancocrud.sqlite';
+
+try {
+    $pdo = new PDO("sqlite:$dbFile");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão com banco de dados realizada com sucesso!\n";
+} catch (PDOException $e) {
+    echo "Falha ao conectar com o banco de dados. Código de erro: " . $e->getMessage() . "\n";
+    die();
+}
